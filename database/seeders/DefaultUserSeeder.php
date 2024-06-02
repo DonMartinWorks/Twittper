2b26827eb3eef041cacf0a8b1e75ad0afe7aad53
DonMartinWorks <donmartinworks@gmail.com>
2024-06-02 14:31:28 -0400

    [USUARIO DEFECTO] - Creación de seeder

---
+++ database/seeders/DefaultUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DefaultUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'User Admin',
+            'email' => 'test@example.com',
+            'password' => Hash::make('123'),
+            'remember_token' => null
+        ]);
+    }
+}
